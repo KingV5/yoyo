90a7304a6213eb02713e582e6c10bd61d7910a50
KingV5 <jeremiahelpro@gmail.com>
2026-03-16 19:20:34 +0000

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,4 +1,3 @@
-
 local isfile = isfile or function(file)
 	local suc, res = pcall(function()
 		return readfile(file)
@@ -32,6 +31,12 @@ local function wipeFolder(path)
 		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
 			delfile(file)
 		end
+	end
+end
+
+for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+	if not isfolder(folder) then
+		makefolder(folder)
 	end
 end
 
@@ -69,26 +74,21 @@ local function downloadPremadeProfiles(commit)
     end
 end
 
-for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
-	if not isfolder(folder) then
-		makefolder(folder)
-	end
-end
-
 if not shared.VapeDeveloper then
-	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/poopparty/poopparty')
-	end)
-	local commit = subbed:find('currentOid')
-	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-		wipeFolder('newvape')
-		wipeFolder('newvape/games')
-		wipeFolder('newvape/guis')
-		wipeFolder('newvape/libraries')
-	end
-	writefile('newvape/profiles/commit.txt', commit)
+    local _, subbed = pcall(function()
+        return game:HttpGet('https://github.com/poopparty/poopparty')
+    end)
+    local commit = subbed:find('currentOid')
+    commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+    commit = commit and #commit == 40 and commit or 'main'
+    if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
+        -- wipeFolder('newvape')
+        -- wipeFolder('newvape/games')
+        -- wipeFolder('newvape/guis')
+        -- wipeFolder('newvape/libraries')
+    end
+    downloadPremadeProfiles(commit)
+    writefile('newvape/profiles/commit.txt', commit)
 end
 
 return loadstring(downloadFile('newvape/main.lua'), 'main')()
